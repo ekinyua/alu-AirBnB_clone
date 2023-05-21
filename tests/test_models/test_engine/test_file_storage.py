6a5a5ec6e076ddc4026e9b844a0c66448c7209b5
DavidkingMazimpaka <d.mazimpaka@alustudent.com>
2023-05-21 17:59:21 +0200

    testfile storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Unit test for the Base class"""
+import unittest
+import pep8
+from models.engine.file_storage import FileStorage
+
+
+class Test_FileStorage(unittest.TestCase):
+    """ Test_BaseModel class"""
+
+    def setUp(self):
+        """ Setup data for every test """
+        self.test_file_storage = FileStorage()
+
+    def tearDown(self):
+        """ Clean the dat for the next test """
+        pass
+
+    def test_instance(self):
+        """ Test if an object is an instance from BaseModel """
+        self.assertIsInstance(self.test_file_storage, FileStorage)
+
+    def test_has_attributes(self):
+        """ Test if the class has all the attributes"""
+        self.assertIsNotNone(hasattr(FileStorage, "__file_path"))
+        self.assertIsNotNone(hasattr(FileStorage, '__objects'))
+        self.assertIsNotNone(hasattr(FileStorage, 'all'))
+        self.assertIsNotNone(hasattr(FileStorage, 'new'))
+        self.assertIsNotNone(hasattr(FileStorage, 'save'))
+        self.assertIsNotNone(hasattr(FileStorage, 'reload'))
+
+    def test_pep8_conformance(self):
+        """ Test that we conform to PEP8 """
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(['models/engine/file_storage.py'])
+        self.assertEqual(result.total_errors, 0, "Fix pep8")
+
+    def test_doctring(self):
+        """ Test doctrings """
+        self.assertIsNotNone(FileStorage.__doc__)
+        self.assertIsNotNone(FileStorage.all.__doc__)
+        self.assertIsNotNone(FileStorage.new.__doc__)
+        self.assertIsNotNone(FileStorage.save.__doc__)
+        self.assertIsNotNone(FileStorage.reload.__doc__)
