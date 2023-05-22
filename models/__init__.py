71962d5855c04bba790fc5ee43ceb1a9eccf0981
Vulgence <e.kinyua@alustudent.com>
2023-05-22 19:07:15 +0300

    init

--- models/__init__.py
+++ models/__init__.py
@@ -1,21 +1,6 @@
 #!/usr/bin/python3
-"""The __init__ method for models directory"""
-
+""" __init__.py method for model package """
 from models.engine.file_storage import FileStorage
-from models.base_model import BaseModel
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
-from models.place import Place
 
-# Create a unique FileStorage instance for your application
 storage = FileStorage()
-
-# Call reload() method on this variable
 storage.reload()
-
-classes = {"BaseModel": BaseModel,  "User": User,  "State": State,
-           "City": City, "Amenity": Amenity, "Place": Place,
-           "Review": Review}
